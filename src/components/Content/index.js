ac502d8d91fb5526d68a205777968f92ca7c7f88
HJr Dev <astronomi@gmail.com>
2019-06-21 19:32:16 -0300

    component Content change Stateful

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import API from 'apisauce';
 
 import { Container } from './styles';
 
-const Content = () => (
-  <Container>
-    <div>
-      <p>teste</p>
-    </div>
-  </Container>
-);
+const baseURL = 'https://gitlab.com/bossabox/challenge-fake-api/tree/master';
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  render() {
+    return (
+      <Container>
+        <div>
+          <p>teste</p>
+        </div>
+      </Container>
+    );
+  }
+}
 
 export default Content;
