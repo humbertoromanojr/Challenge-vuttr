1e6498b8e455148887d421025ff386ea0c69cd13
HJr Dev <astronomi@gmail.com>
2019-06-21 10:17:23 -0300

    provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <h1>VUTTR</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>VUTTR</h1>
+      </div>
+    </Provider>
   );
 }
 
