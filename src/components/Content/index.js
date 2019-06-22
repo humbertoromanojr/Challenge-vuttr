0be745cc26bda7f15412ce3ccc951419d62330c2
HJr Dev <astronomi@gmail.com>
2019-06-22 16:36:37 -0300

    config PropTypes

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 import { Creators as ToolsListActions } from '../../store/ducks/toolsList';
 
 import {
@@ -9,6 +10,20 @@ import {
 } from './styles';
 
 class Content extends Component {
+  static propTypes = {
+    getToolsListRequest: PropTypes.func.isRequired,
+    toolsList: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+          description: PropTypes.string,
+          tags: PropTypes.string,
+        }),
+      ),
+    }).isRequired,
+  };
+
   componentDidMount() {
     this.props.getToolsListRequest();
   }
